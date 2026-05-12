08bba0f5f496a18b6a6f1262c79bd2ef0c720b66
Justin Hogue <jhogue26@ivytech.edu>
2026-05-12 19:29:57 -0400

    django project changes

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,4 @@
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("<h1>Hello, this is my Django assignment!</h1>")
-
 def post_list(request):
     return HttpResponse("<h1>Post List</h1>")
