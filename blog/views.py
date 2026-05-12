a704e5bbbee063efdd3db0ee8af19a8bd5316048
Justin Hogue <jhogue26@ivytech.edu>
2026-05-12 19:17:24 -0400

    django project setup

--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1>Hello, this is my Django assignment!</h1>")
+
+def post_list(request):
+    return HttpResponse("<h1>Post List</h1>")
