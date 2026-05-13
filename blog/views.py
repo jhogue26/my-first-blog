c574312b0887c04ad69460b53bdf56696b655614
Justin Hogue <jhogue26@ivytech.edu>
2026-05-12 21:28:57 -0400

    complete Module 5 tutorial

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def post_list(request):
-    return HttpResponse("<h1>Post List</h1>")
+    return render(request, 'blog/post_list.html', {})
